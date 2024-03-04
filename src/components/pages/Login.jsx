718b9db3d8db5c37723f01aa58d517be471e9b95
AF0ns3ca <afonsecahdez@gmail.com>
2024-03-04 09:55:51 +0100

    instalacion de prettier

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,20 +1,32 @@
 import axios from "axios";
 import { API_URL } from "../../constants/env";
 import { setToken } from "../../helpers/auth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+   const nav = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            password: e.target.password.value,
-        };
-        axios.post(`${API_URL}/public/login`, data)
-            .then(resp => setToken(resp.data.data.token))
-            .catch(err => console.log(err));
-    };
+   //Manejo del estado de errores
+   const [error, setError] = useState();
 
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError(null);
+
+      const data = {
+         email: e.target.email.value,
+         password: e.target.password.value,
+      };
+      axios
+         .post(`${API_URL}/public/login`, data)
+         .then((resp) => {
+            setToken(resp.data.token)
+            nav("/");
+         })
+         .catch((err) => console.log(err));
+   };
 
    return (
       <div className="pt-16 max-w-256 m-auto">
